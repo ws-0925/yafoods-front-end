3d170a8a234290f1e2f9211e4acaf139fbde2f98
superdev <star.software.guy@gmail.com>
2023-01-04 23:14:27 -0800

    fix the city management.

--- src/pages/location/city/index.tsx
+++ src/pages/location/city/index.tsx
@@ -12,11 +12,7 @@ import { DataGrid } from '@mui/x-data-grid'
 import MenuItem from '@mui/material/MenuItem'
 import Menu from '@mui/material/Menu'
 import CardHeader from '@mui/material/CardHeader'
-import InputLabel from '@mui/material/InputLabel'
 import { IconButton } from '@mui/material'
-import FormControl from '@mui/material/FormControl'
-import CardContent from '@mui/material/CardContent'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
 import Typography from '@mui/material/Typography'
 
 // ** Icon Imports
@@ -40,18 +36,6 @@ import TableHeader from 'src/views/apps/city/TableHeader'
 import { CityType } from 'src/types/apps/cityType'
 import AddCityDrawer from 'src/views/apps/city/AddCityDrawer'
 
-interface viewDataType {
-  totalCity: number
-  activeCity: number
-  inactiveCity: number
-}
-
-const viewData: viewDataType = {
-  totalCity: 37,
-  activeCity: 25,
-  inactiveCity: 12
-}
-
 interface CityStatusType {
   [key: string]: ThemeColor
 }
@@ -67,7 +51,6 @@ const cityStatusList: CityStatusType = {
 
 const CityList = () => {
   // ** State
-  const [status, setStatus] = useState<string>('')
   const [value, setValue] = useState<string>('')
   const [filterData, setFilterData] = useState<any>([])
   const [isFirst, setIsFirst] = useState<boolean>(true)
@@ -81,21 +64,6 @@ const CityList = () => {
   useEffect(() => {
     dispatch(getCities())
   }, [dispatch])
-
-  const handleStatusChange = useCallback(
-    (e: SelectChangeEvent) => {
-      setIsFirst(false)
-      if (e.target.value == '') {
-        setFilterData(cities)
-
-        return
-      }
-      const data = cities.filter((item: { status: string }) => item.status == e.target.value)
-      setFilterData(data)
-      setStatus(e.target.value)
-    },
-    [cities]
-  )
 
   const handleFilter = useCallback(
     (val: string) => {
@@ -189,13 +157,13 @@ const CityList = () => {
       minWidth: 110,
       field: 'status',
       headerName: 'Status',
-      renderCell: ({ row }: CellType) => {
+      renderCell: () => {
         return (
           <CustomChip
             skin='light'
             size='small'
-            label={row.status}
-            color={cityStatusList[row.status]}
+            label={'active'}
+            color={cityStatusList['active']}
             sx={{ textTransform: 'capitalize', '& .MuiChip-label': { lineHeight: '18px' } }}
           />
         )
@@ -214,63 +182,8 @@ const CityList = () => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
-        {viewData && (
-          <Grid container spacing={6}>
-            <Grid item xs={12} md={4}>
-              <Card sx={{ border: 0, boxShadow: 0, color: 'common.white', backgroundColor: '#16B1FF', height: '100%' }}>
-                <CardContent sx={{ py: theme => `${theme.spacing(4.125)} !important` }}>
-                  <Typography variant='h5' sx={{ color: 'common.white', textAlign: 'center' }}>
-                    Total City: {viewData.totalCity}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Card sx={{ border: 0, boxShadow: 0, color: 'common.white', backgroundColor: '#d43b48', height: '100%' }}>
-                <CardContent sx={{ py: theme => `${theme.spacing(4.125)} !important` }}>
-                  <Typography variant='h5' sx={{ color: 'common.white', textAlign: 'center' }}>
-                    Active City: {viewData.activeCity}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Card sx={{ border: 0, boxShadow: 0, color: 'common.white', backgroundColor: '#79eb6a', height: '100%' }}>
-                <CardContent sx={{ py: theme => `${theme.spacing(4.125)} !important` }}>
-                  <Typography variant='h5' sx={{ color: 'common.white', textAlign: 'center' }}>
-                    Inactive City: {viewData.inactiveCity}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid>
-        )}
-      </Grid>
-      <Grid item xs={12}>
         <Card>
-          <CardHeader title='Search Filters' sx={{ pb: 4, '& .MuiCardHeader-title': { letterSpacing: '.15px' } }} />
-          <CardContent>
-            <Grid container spacing={6}>
-              <Grid item sm={4} xs={12}>
-                <FormControl fullWidth>
-                  <InputLabel id='status-select'>Select Status</InputLabel>
-                  <Select
-                    fullWidth
-                    value={status}
-                    id='select-status'
-                    label='Select Status'
-                    labelId='status-select'
-                    onChange={handleStatusChange}
-                    inputProps={{ placeholder: 'Select Status' }}
-                  >
-                    <MenuItem value=''>Select Status</MenuItem>
-                    <MenuItem value='active'>Active</MenuItem>
-                    <MenuItem value='inactive'>Inactive</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-            </Grid>
-          </CardContent>
+          <CardHeader title='City Management' sx={{ pb: 4, '& .MuiCardHeader-title': { letterSpacing: '.15px' } }} />
           <Divider />
           <TableHeader value={value} handleFilter={handleFilter} toggle={toggleAddUserDrawer} />
           <DataGrid
